c6e95aa5ab013eb0d02ff6c11b60209918fafa2b
Mathieu Noverraz <mathieu.noverraz@gmail.com>
2019-04-09 16:37:19 +0200

    feature(install): add tmux base installation

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ install_dotfile_git_repo(){
     mkdir ~/code/
     git clone https://github.com/mnoverraz/dotfiles.git ~/code/
 }
+
+install_tmux_config(){
+    ln -s ~/code/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+}
